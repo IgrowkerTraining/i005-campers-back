dc6aeeab69f3e18a2166c555defce3438732fc68
Lucas Pirez <lucaspirez42@gmail.com>
2025-04-11 10:50:14 -0300

    Add Reservations Module

--- src/modules/reservations/dto/create-reservation.dto.ts
+++ src/modules/reservations/dto/create-reservation.dto.ts
@@ -1,9 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Reservation } from '@prisma/client';
 
 export class CreateReservationDto implements Omit<Reservation, 'id' | 'createdAt' | 'updatedAt'> {
   campingId: number;
-  startDate: Date;
-  endDate: Date;
   peopleCount: number;
   tentsCount: number;
+  @ApiProperty({
+    type: String,
+    format: 'date',
+    description: 'Fecha sin hora (YYYY-MM-DD)',
+  })
+  startDate: Date;
+  @ApiProperty({
+    type: String,
+    format: 'date',
+    description: 'Fecha sin hora (YYYY-MM-DD)',
+  })
+  endDate: Date;
 }
