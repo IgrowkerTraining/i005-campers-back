8848b4827ca01a214418aedabbce49556731abe3
Lucas Pirez <lucaspirez42@gmail.com>
2025-04-16 10:19:20 -0300

    Update Dto

--- src/modules/reservations/dto/create-reservation.dto.ts
+++ src/modules/reservations/dto/create-reservation.dto.ts
@@ -1,19 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Reservation } from '@prisma/client';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateReservationDto
   implements Omit<Reservation, 'id' | 'createdAt' | 'updatedAt' | 'cancelledAt' | 'status'>
 {
+  @IsNotEmpty()
   userId: string;
+  @IsNotEmpty()
   campingId: number;
+  @IsNotEmpty()
   peopleCount: number;
+  @IsNotEmpty()
   tentsCount: number;
+  @IsNotEmpty()
   @ApiProperty({
     type: String,
     format: 'date',
     description: 'Fecha sin hora (YYYY-MM-DD)',
   })
   startDate: Date;
+  @IsNotEmpty()
   @ApiProperty({
     type: String,
     format: 'date',
