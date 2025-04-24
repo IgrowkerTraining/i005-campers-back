c02a7429c988035e6acfe752964bdc2e061fe40c
Eduardolans <141261251+Eduardolans@users.noreply.github.com>
2025-04-24 21:35:49 +0200

    remove unused code

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,34 +1,3 @@
-// import { Injectable, OnModuleInit } from '@nestjs/common';
-// import { PrismaClient } from '@prisma/client';
-
-// @Injectable()
-// export class PrismaService extends PrismaClient implements OnModuleInit {
-//   async onModuleInit() {
-//     await this.$connect();
-//   }
-// }
-
-// import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-// import { PrismaClient } from '@prisma/client';
-
-// @Injectable()
-// export class PrismaService extends PrismaClient implements OnModuleInit {
-//   async onModuleInit() {
-//     try {
-//       await this.$connect();
-//     } catch (e) {
-//       console.error('Failed to connect to the database:', e);
-//       process.exit(1);
-//     }
-//   }
-
-//   async enableShutdownHooks(app: INestApplication) {
-//     this.$on('beforeExit', async () => {
-//       await app.close();
-//     });
-//   }
-// }
-
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
